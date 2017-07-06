80681bbe190556f6186d9cea32c34942cc142581
ridickle <ridickle7@gmail.com>
2017-07-06 20:08:59 +0900

    Write Chapter
    1) write 1-1 Static Factory Method
    2) start writing 1-2 builder

---
+++ src/Object_1/builder/builder_1_2.java
@@ -0,0 +1,7 @@
+package Object_1.builder;
+
+/**
+ * Created by ridickle on 2017. 7. 6..
+ */
+public class builder_1_2 {
+}
--- src/Object_1/staticFactory/staticFactory_1_1.java
+++ src/Object_1/staticFactory/staticFactory_1_1.java
@@ -4,5 +4,28 @@ package Object_1.staticFactory;
  * Created by ridickle on 2017. 7. 5..
  */
 public class staticFactory_1_1 {
+    // 장점 4를 활용한 string 별 객체 생성
+    Blog a = getInstance("A");
+    public static Blog getInstance(String type){
+        if(type.equals("A")){
+            return new A();
+        }
+        else if(type.equals("B")){
+            return new B();
+        }
+        return null;
+    }
+}
+
+
+class Blog{
+
+}
+
+class A extends Blog{
+
+}
+
+class B extends Blog{
 
 }
