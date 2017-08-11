56cd9726baa99792b9aff1462993d2ca2cf616d6
ridickle <ridickle7@gmail.com>
2017-08-11 21:35:15 +0900

    Update Chapter
    1) 04_private (Completed)
    2) 05_useless (Started)

--- src/Object_1/_04_private/private_1_4.java
+++ src/Object_1/_04_private/private_1_4.java
@@ -5,15 +5,20 @@ package Object_1._04_private;
  */
 public class private_1_4 {
 
-    UtilityClass instance = UtilityClass(); // 에러 발생 (
+    UtilityClass instance = UtilityClass(); // 생성이 안됨 (오류코드)
 }
 
 
 class UtilityClass {
-    // 디폴트 생성자가 만들어지는것을 방지
-    private UtilityClass instance;
+    // private 선언을 통해 생성자가 만들어지는것을 방지
     private UtilityClass() {
         // 혹시나 불러질 가능성 대비해서 (클래스 내부에서) 에러 발생
         throw new AssertionError();
     }
+}
+
+class UtilSubClass extends UtilityClass{
+
+    // 부모 클래스 생성자가 private라 자식 클래스도 그 영향을 받는다.
+    private UtilSubClass() {}    // (오류코드)
 }
---
+++ src/Object_1/_05_uselessObject/useless_1_5.java
@@ -0,0 +1,7 @@
+package Object_1._05_uselessObject;
+
+/**
+ * Created by ridickle on 2017. 8. 11..
+ */
+public class useless_1_5 {
+}
